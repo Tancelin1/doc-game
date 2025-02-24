3eee747aece584b089412744c7b799cdf7c1feff
Tancelin1 <77938295+Tancelin1@users.noreply.github.com>
2025-02-24 12:26:35 +0100

    test config

--- forge.config.ts
+++ forge.config.ts
@@ -2,6 +2,7 @@ import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerDeb } from '@electron-forge/maker-deb';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
@@ -18,7 +19,7 @@ const config: ForgeConfig = {
     }),
     new MakerZIP({}, ['darwin']),
     new MakerRpm({}),
-    // Retiré MakerDeb
+    new MakerDeb({}),
   ],
   plugins: [
     new VitePlugin({
